26b8bc586ef4edc96ed036fc0e7831d3862ee231
sajeed786 <hossains018@gmail.com>
2021-02-01 00:21:07 +0530

    modified security key settings

--- EventManagement/EventManagement/settings.py
+++ EventManagement/EventManagement/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5ab*w!aiv_$dfn%@t-kjsd1o_uc^h7&0t!v0-_an7%t9n0%g=6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,8 +29,8 @@ DEBUG = True
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'eventmanagingplatform@gmail.com'
-EMAIL_HOST_PASSWORD = 'Thehost@111'
+EMAIL_HOST_USER = os.environ.get('SMTP_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_HOST_PASS')
 EMAIL_PORT = 587
 
 ALLOWED_HOSTS = []
